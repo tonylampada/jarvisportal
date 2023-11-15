ee46ad8ea4fe59c644f6a609b705456425a0e61f
Tony Lampada <tonylampada@gmail.com>
2023-11-15 19:27:09 -0300

    Add test for action_updateFile method

--- tests/test_gptexec.py
+++ tests/test_gptexec.py
@@ -1,6 +1,20 @@
 import pytest
-from gptexec import action_exec
+import os
+from gptexec import action_exec, action_updateFile
 
 def test_action_exec():
     result = action_exec('echo "Hello, World!"')
     assert result['output'] == 'Hello, World!\n'
+
+def test_action_updateFile():
+    test_content = 'Test content'
+    test_path = 'testfile.txt'
+    # Write test content to a file
+    action_updateFile(test_path, test_content)
+    # Read content to verify
+    with open(test_path, 'r') as file:
+        content = file.read()
+    # Clean up test file
+    os.remove(test_path)
+    # Assert test content is written correctly
+    assert content == test_content
