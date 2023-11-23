1c722d52db0c8f872a2196927b525c2198329eec
Tony Lampada <tonylampada@gmail.com>
2023-11-23 07:46:31 -0300

    client auth

--- client.py
+++ client.py
@@ -3,14 +3,37 @@ import websockets
 import json
 import actions
 from random import randint
+import os
+import requests
 
+BASE_DOMAIN = "s://gptexec.evolutio.io"
+# BASE_DOMAIN = "://localhost:8000"
 
 async def test_websocket():
-    user_id = "tony"
-    # uri = "ws://127.0.0.1:8000/ws"  # Substitua pela URL do seu WebSocket
-    uri = "wss://gptexec.evolutio.io/ws"  # Substitua pela URL do seu WebSocket
+    userhome = os.path.expanduser("~")
+    configfilepath = f"{userhome}/.jarvis-config.json"
+    device_key = None
+    if not os.path.exists(configfilepath):
+        user_devicekey = input("Enter your devices key: ")
+        if not user_devicekey:
+            print("No devices key provided. Exiting...")
+            exit(1)
+        device_name = input("Enter a device name: ")
+        if not device_name:
+            print("No device name provided. Exiting...")
+            exit(1)
+        device_description = input("Add a device description (optional): ")
+        device_key = register_device(user_devicekey, device_name, device_description)
+        with open(configfilepath, "w") as file:
+            json.dump({"device_key": device_key}, file)
+    else:
+        with open(configfilepath, "r") as file:
+            config = json.load(file)
+            device_key = config["device_key"]
+    uri = f"ws{BASE_DOMAIN}/ws"  # Substitua pela URL do seu WebSocket
     async with websockets.connect(uri) as websocket:
-        await websocket.send(user_id)
+        await websocket.send(device_key)
+        print("Connected. waiting for commands...")
         while True:
             msg = json.loads(await websocket.recv())
             action = {
@@ -20,8 +43,27 @@ async def test_websocket():
                     "arguments": msg["arguments"]
                 }
             }
-            actionresult = actions._exec_action(action)
+            actionresult = actions.exec_action(action)
             await websocket.send(json.dumps(actionresult))
 
-# Executar a função de teste do WebSocket
-asyncio.get_event_loop().run_until_complete(test_websocket())
+def register_device(user_devicekey, device_name, device_description):
+    uri = f"http{BASE_DOMAIN}/api/registerdevice"
+    data = {
+        "user_devicekey": user_devicekey,
+        "device_name": device_name,
+        "device_description": device_description
+    }
+    r = requests.post(uri, json=data)
+    if r.status_code != 200:
+        print(f"Error registering device: {r.status_code} - {r.text}")
+        exit(1)
+    result = r.json()
+    if not result["success"]:
+        print(f"Error registering device: {result['message']}")
+        exit(1)
+    device_key = result["key"]
+    return device_key
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(test_websocket())
